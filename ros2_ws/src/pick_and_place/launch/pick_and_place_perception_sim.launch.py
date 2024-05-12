f5c81e0d0001878035b969f5b94d89e3d19d4445
mefisto2017 <velaenator@gmail.com>
2024-05-12 22:54:30 +0800

    load scene in launch file from .scene file

--- ros2_ws/src/pick_and_place/launch/pick_and_place_perception_sim.launch.py
+++ ros2_ws/src/pick_and_place/launch/pick_and_place_perception_sim.launch.py
@@ -2,6 +2,8 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
@@ -10,13 +12,13 @@ from moveit_configs_utils import MoveItConfigsBuilder
 
 def generate_launch_description():
     # Initialize Arguments
-    moveit_package = LaunchConfiguration("my_moveit_config")
-    ur_package = LaunchConfiguration("ur_description")
+    moveit_package = get_package_share_directory("my_moveit_config")
+    ur_package = get_package_share_directory("ur_description")
 
     # Find paths
-    scene_file_path = PathJoinSubstitution([FindPackageShare(moveit_package), "scenes", "coffee_shop.scene"])
-    srdf_file_path = PathJoinSubstitution([FindPackageShare(moveit_package), "config", "name.srdf"])
-    urdf_file_path = PathJoinSubstitution([FindPackageShare(ur_package), "urdf", "ur.urdf.xacro"]) 
+    scene_file_path = PathJoinSubstitution([moveit_package, "scenes", "coffee_shop.scene"])
+    srdf_file_path = PathJoinSubstitution([moveit_package, "config", "name.srdf"])
+    urdf_file_path = PathJoinSubstitution([ur_package, "urdf", "ur.urdf.xacro"]) 
     
     moveit_config = MoveItConfigsBuilder("name", package_name="my_moveit_config").to_moveit_configs()
 
@@ -36,16 +38,12 @@ def generate_launch_description():
         package="moveit_ros_planning",
         executable="moveit_publish_scene_from_text",
         output="screen",
-        parameters=[
-            {'robot_description': urdf_file_path},
-            {'robot_description_semantic':srdf_file_path}
-        ],
-        remappings=[
-            ('__node:=moveit_publish_scene_from_text')
-        ],
-        arguments=[scene_file_path]
+        parameters=[],
+        arguments=[
+            '--scene', scene_file_path]
     )
     
     return LaunchDescription([
-        ppp_node
+        ppp_node,
+        scene_pub
     ])
