8109e43229b31cdff39cca290f267fe9a062974a
Pablo <velaenator@gmail.com>
2024-05-21 10:13:10 +0800

    real arm controllers added

---
+++ ros2_ws/src/ur_arm/real_my_moveit_config/launch/move_group.launch.py
@@ -0,0 +1,41 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # moveit_config = MoveItConfigsBuilder("name", package_name="my_moveit_config").to_moveit_configs()
+
+    moveit_config = (
+        MoveItConfigsBuilder("name", package_name="my_moveit_config")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .moveit_cpp(file_path="config/moveit_cpp.yaml")
+        .to_moveit_configs()
+    )
+    
+    # Move Group Node
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"trajectory_execution.allowed_execution_duration_scaling": 2.0,},
+            {"publish_robot_description_semantic": True},
+            {"use_sim_time": False},
+        ],
+    )
+
+    scene_planner_node = Node(
+        package="moveit_ros_planning",
+        executable="moveit_publish_scene_from_text",
+        output="screen",
+    )
+
+    return LaunchDescription(
+        [move_group_node]
+    )
